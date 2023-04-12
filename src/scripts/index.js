a343bb0249e8fe319ae38974c6d6848e290e3ab3
denisesantosdev <denisesantosdev@gmail.com>
2023-04-11 23:00:19 -0300

    refatoração

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,6 @@
+import { user } from "/src/scripts/services/user.js";
+import { repositories } from "/src/scripts/services/repositories.js";
+
 document.getElementById("btn-search").addEventListener("click", () => {
   const userName = document.getElementById("input-search").value;
   getUserProfile(userName);
@@ -12,23 +15,9 @@ document.getElementById("input-search").addEventListener("keyup", (e) => {
   }
 });
 
-async function user(userName) {
-  const response = await fetch(`https://api.github.com/users/${userName}`);
-  return await response.json();
-}
-
-async function repos(userName) {
-  const response = await fetch(`https://api.github.com/users/${userName}/repos?per_page=10`);
-  //console.log(response);
-  return await response.json();
-}
-
-//console.log(await user());
-
 function getUserProfile(userName) {
 
   user(userName).then((userData) => {
-    //console.log(userData);
     let userInfo = `
     <div class="info">
         <img src="${userData.avatar_url} alt="Foto do perfil do usuário"/>
@@ -45,19 +34,18 @@ function getUserProfile(userName) {
 }
 
 function getUserRepositories(userName) {
-  repos(userName).then((reposData) => {
-    // console.log(reposData);
+  repositories(userName).then((reposData) => {
 
-    let repositoriesItens = "";
+    let repositoriesItems = "";
 
     reposData.forEach((repo) => {
-      repositoriesItens += `<li><a href="${repo.html_url}">${repo.name}</a></li>`;
+      repositoriesItems += `<li><a href="${repo.html_url}">${repo.name}</a></li>`;
     });
 
     document.querySelector(".profile-data").innerHTML += `
     <div class="repositories section">
         <h2>Repositórios</h2>
-        <ul>${repositoriesItens}</ul>
+        <ul>${repositoriesItems}</ul>
     </div>
         `;
   });
